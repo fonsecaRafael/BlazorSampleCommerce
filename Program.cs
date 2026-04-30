b70319c8e4fb22a5b4203f25fd7ecc5d5844ea34
fonsecaRafael <rafael.abreu.fonseca@gmail.com>
2026-04-30 15:18:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using BlazorSampleCommerce.Components;
+using BlazorSampleCommerce.Services;
+
+namespace BlazorSampleCommerce
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+            builder.Services.AddScoped<AuthService>();
+            builder.Services.AddScoped<SearchService>();
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7082/")
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
---
+++ Services/AuthService.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorSampleCommerce.Services
+{
+    public class AuthService
+    {
+        public bool IsLoggedIn { get; set; } = false;
+        public string UserEmail { get; set; } = "";
+
+        public void Login(string email)
+        {
+            IsLoggedIn = true;
+            UserEmail = email;
+        }
+
+        public void Logout()
+        {
+            IsLoggedIn = false;
+            UserEmail = "";
+        }
+    }
+}
